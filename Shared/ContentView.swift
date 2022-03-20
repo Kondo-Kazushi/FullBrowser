ab5cae44f641a4a764b256f9fb1cef307da5a10c
Kazu <romans121718kk@icloud.com>
2022-03-20 19:52:16 +0900

    Commit!!

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var theURL = "https://"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            VStack{
+                TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: $theURL)
+                    .keyboardType(.URL)
+                    .textInputAutocapitalization(.never)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                NavigationLink("Open", destination: WebpageView(loadUrl: theURL)
+                    .edgesIgnoringSafeArea(.all)
+                    .navigationBarTitle("")
+                    .navigationBarHidden(true)
+                )
+                
+            }.padding()
+        }.navigationViewStyle(StackNavigationViewStyle())
+        
+        
+        
+        
+        
     }
 }
 
---
+++ Shared/Web.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import WebKit
+
+struct WebpageView: UIViewRepresentable {
+    var loadUrl:String
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        uiView.load(URLRequest(url: URL(string: loadUrl)!))
+    }
+}
