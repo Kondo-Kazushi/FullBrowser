a7cba3965892e3897752b38af9d8a11dad33c66b
Kazu <romans121718kk@icloud.com>
2022-03-20 18:37:15 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 近藤和志 on 2022/03/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/FullBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullBrowserApp.swift
+//  Shared
+//
+//  Created by 近藤和志 on 2022/03/20.
+//
+
+import SwiftUI
+
+@main
+struct FullBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
